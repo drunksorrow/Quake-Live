15f7bc7fbe2391f62a5db867f6cd87a697c3a447
drunksorrow <69868026+drunksorrow@users.noreply.github.com>
2020-08-21 22:51:30 +0300

    Update quakestartsingle.sh

--- qlds-scripts/quakestartsingle.sh
+++ qlds-scripts/quakestartsingle.sh
@@ -15,14 +15,14 @@ cd ~/steamcmd/steamapps/common/qlds/baseq3
 exec $qPathToStartScript \
     +set net_strict 1 \
     +set net_port $gameport \
-    +set sv_hostname "My Quake Server Name" \
+    +set sv_hostname "Drunk Test Server" \
     +set zmq_rcon_enable 1 \
     +set zmq_rcon_password "password" \
     +set zmq_rcon_port $rconport \
     +set zmq_stats_enable 1 \
     +set zmq_stats_password "password" \
     +set zmq_stats_port $gameport \
-    +set sv_tags "My Server Tag, My Server Location" \
+    +set sv_tags "drunk,test,ffa,single" \
     +set g_voteFlags "0" \
     +set g_accessFile "access.txt" \
     +set sv_mappoolFile "mappool.txt" \
